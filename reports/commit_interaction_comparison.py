a4ad714618585408fc17274beae9290f560f4f55
Gabrian Giovano Tanasal <gabriantanasal@gmail.com>
2026-09-14 17:04:25 +0200

    fix path for experiment

--- reports/commit_interaction_comparison.py
+++ reports/commit_interaction_comparison.py
@@ -16,7 +16,7 @@ from varats.data.reports.blame_interaction_graph import (
 from varats.experiments.vara.blame_report_experiment import (
     BlameReportExperiment,
 )
-from experiments.vara.cfg_report_experiment import (
+from experiments.cfg_report_experiment import (
     CFCollectiveReportExperiment,
     CFDirectReportExperiment,
 )
